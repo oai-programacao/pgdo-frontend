9bb7659c3e8f8e6f2c580c9e92480d887f78034b
Diego Soares <dsadamovich@gmail.com>
2025-08-09 11:03:18 -0300

    feat: atualiza a URL da API para o ambiente de desenvolvimento

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://apipgdo.oai.com.br/api',
+    apiUrl: 'http://localhost:8080/api',
     googleMapsApiKey: 'AIzaSyCmavFR3WM6LhreBeGEBSKr5nKBAHcWVms'
 }
-
-
-// https://apipgdo.oai.com.br/api
-
-// http://localhost:8080/api
