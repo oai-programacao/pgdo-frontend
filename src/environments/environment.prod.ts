3cf3053d0eafc2501a5685361937d9cb076e97e9
Diego Soares <dsadamovich@gmail.com>
2025-08-27 18:41:56 -0300

    adiciona url e muda alguns operadores pra ver se roda

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
     apiUrl: 'https://apipgdo.oai.com.br/api',
+    wsUrl: "ws://apipgdo.oai.com.br/api/ws-connect",
     logLevel: 'error', // Define o nível de log para produção
     googleMapsApiKey: 'AIzaSyCmavFR3WM6LhreBeGEBSKr5nKBAHcWVms'
 }
