cd4cbe2edc53a44a3c737302f8bc539ad6e76d1d
Diego Soares <dsadamovich@gmail.com>
2025-09-01 17:23:49 -0300

    finalizando wss para funcionar

--- src/app/core/websocket/wsStompConfig.ts
+++ src/app/core/websocket/wsStompConfig.ts
@@ -1,7 +1,7 @@
 import SockJS from "sockjs-client";
 
 export const wsStompConfig = {
-    webSocketFactory: () => new SockJS("http://localhost:8080/api/ws-connect"),
+    webSocketFactory: () => new SockJS("https://apipgdo.oai.com.br:8080/api/ws-connect"),
     heartbeatIncoming: 0,
     heartbeatOutgoing: 20000,
     reconnectDelay: 5000,
