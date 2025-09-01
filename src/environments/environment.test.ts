a700269d16ef2219717e40343dffe60f116dcc50
Diego Soares <dsadamovich@gmail.com>
2025-09-01 17:11:44 -0300

    fix: update WebSocket URLs to use secure wss protocol

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: "http://187.33.200.196:8080/api",
-  wsUrl: "ws://187.33.200.196:8080/api/ws-connect", /*testar pipeline*/
+  wsUrl: "wss://187.33.200.196:8080/api/ws-connect", /*testar pipeline*/
   googleMapsApiKey: "AIzaSyCmavFR3WM6LhreBeGEBSKr5nKBAHcWVms",
 };
