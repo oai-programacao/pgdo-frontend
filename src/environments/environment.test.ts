82dcfb9c605a62cbdefef61749bf92c2d1523adb
Diego Soares <dsadamovich@gmail.com>
2025-09-01 17:15:35 -0300

    fix: update WebSocket URL to use non-secure ws protocol and remove test environment configuration

--- src/environments/environment.test.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: "http://187.33.200.196:8080/api",
-  wsUrl: "wss://187.33.200.196:8080/api/ws-connect", /*testar pipeline*/
-  googleMapsApiKey: "AIzaSyCmavFR3WM6LhreBeGEBSKr5nKBAHcWVms",
-};
